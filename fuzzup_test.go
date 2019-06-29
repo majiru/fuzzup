fa8f3c37120cad3f90f80f3cbfbf973ceaef28a6
Jacob Moody <jsmoody@iastate.edu>
2019-06-29 14:12:54 -0500

    Added tests for read and fetch

--- fuzzup_test.go
+++ fuzzup_test.go
@@ -3,6 +3,10 @@ package fuzzup
 import (
 	"fmt"
 	"testing"
+	"strings"
+	"bufio"
+	"net/http"
+	"net/http/httptest"
 )
 
 
@@ -14,5 +18,93 @@ func TestBuildURL(t *testing.T) {
 	result := buildURL(outter, inner)
 	if result !=  expected {
 		t.Errorf("Expected %s but got %s", expected, result)
+	}
+}
+
+func TestRead(t *testing.T) {
+	const fuzzin = "a\tb\nc\td\ne\tf\n"
+	target := "https://example.com?{{}}=test&test2={{}}"
+	r := bufio.NewScanner(strings.NewReader(fuzzin))
+	out := make(chan string)
+	errc := make(chan error)
+	go func() {
+		read(strings.Split(target, "{{}}"), r, out, errc)
+		close(errc)
+	}()
+	go func() {
+		for err := range errc {
+			t.Errorf("Got error %s when reading", err.Error())
+		}
+	}()
+	i := 0
+	for url := range out {
+		switch i {
+		case 0:
+			const e = "https://example.com?a=test&test2=b"
+			if url != e {
+				t.Errorf("Expected %s got %s", e, url)
+			}
+		case 1:
+			const e = "https://example.com?c=test&test2=d"
+			if url != e {
+				t.Errorf("Expected %s got %s", e, url)
+			}
+		case 2:
+			const e = "https://example.com?e=test&test2=f"
+			if url != e {
+				t.Errorf("Expected %s got %s", e, url)
+			}
+		}
+		i++
+	}
+}
+
+func TestFetch(t *testing.T) {
+	testPages := []string{"Test", "Test2", "uniq", "other"}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			switch incoming := strings.Trim(r.URL.String(), "/"); incoming {
+			case testPages[0]:
+				fmt.Fprintln(w, "Test")
+			case testPages[1]:
+				fmt.Fprintln(w, "Test")
+			case testPages[2]:
+				fmt.Fprintln(w, "Test2")
+			case testPages[3]:
+				fmt.Fprintln(w, "Test3")
+			default:
+				t.Errorf("Unknown request %s", incoming)
+			}
+	}))
+	defer ts.Close()
+	in := make(chan string)
+	out := make(chan Record)
+	errc := make(chan error)
+	go func(){
+		for err := range errc {
+			t.Errorf("%q", err)
+		}
+	}()
+	go fetch("", in, out, errc)
+	go func() {
+		for _, page := range testPages {
+			in <- fmt.Sprintf("%s/%s", ts.URL, page)
+		}
+		close(in)
+	}()
+	i := 0
+	for r := range out {
+		eurl := fmt.Sprintf("%s/%s", ts.URL, testPages[i])
+		e := Record{url: eurl}
+		switch i {
+		case 0, 2, 3:
+			e.count = 1
+		case 1:
+			e.url = fmt.Sprintf("%s/%s", ts.URL, testPages[0])
+			e.count = 2
+		}
+		if e != r {
+			t.Errorf("Expected %q got %q", e, r)
+		}
+		i++
 	}
 }
