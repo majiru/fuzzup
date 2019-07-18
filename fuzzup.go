2858002aa4b577e55c550e13c285e3522b6d3b79
Jacob Moody <jsmoody@iastate.edu>
2019-07-18 13:50:31 -0500

    Seperated http request and hashing Moved ledger to global *sync.Map Refactor test to reflect change Added hashproc tests

--- fuzzup.go
+++ fuzzup.go
@@ -10,7 +10,17 @@ import (
 	"net/http"
 	"regexp"
 	"strings"
+	"sync"
 )
+
+type Record struct {
+	url   string
+	b     []byte
+	count int
+}
+
+//map[string]*Record
+var ledger sync.Map = sync.Map{}
 
 func buildURL(outter []string, inner []string) string {
 	var s string
@@ -20,69 +30,75 @@ func buildURL(outter []string, inner []string) string {
 	return s + outter[len(outter)-1]
 }
 
-func read(target []string, scanner *bufio.Scanner, c chan string, errc chan error) {
+func readproc(target []string, scanner *bufio.Scanner, c chan *Record, errc chan error) {
 	for i := 1; scanner.Scan(); i++ {
 		parts := strings.Split(scanner.Text(), "\t")
 		if len(parts) != len(target)-1 {
 			errc <- fmt.Errorf("Line %d: Expected %d fields, line has %d", i, len(target)-1, len(parts))
 			continue
 		}
-		c <- buildURL(target, parts)
+		c <- &Record{url: buildURL(target, parts)}
 	}
 	close(c)
 }
 
-type Record struct {
-	url   string
-	count int
-}
-
-func fetch(filter string, in chan string, out chan Record, errc chan error) {
+func fetchproc(in chan *Record, out chan *Record, errc chan error) {
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 	client := &http.Client{Transport: tr}
-	ledger := make(map[string]Record)
-	rxp := regexp.MustCompile(filter)
-	for url := range in {
-		r, err := client.Get(url)
+	for rec := range in {
+		r, err := client.Get(rec.url)
 		if err != nil {
 			errc <- err
 			continue
 		}
-		b, err := ioutil.ReadAll(r.Body)
+		rec.b, err = ioutil.ReadAll(r.Body)
 		if err != nil {
 			errc <- err
 			continue
 		}
 		r.Body.Close()
-		b = rxp.ReplaceAll(b, []byte(""))
-		hashStr := fmt.Sprintf("%x", sha256.Sum256(b))
-		if rec, ok := ledger[hashStr]; ok {
-			rec.count++
-			out <- rec
-		} else {
-			rec = Record{url, 1}
-			ledger[hashStr] = rec
-			out <- rec
+		out <- rec
+	}
+	close(out)
+}
+
+func hashproc(filter string, in chan *Record, out chan *Record, errc chan error) {
+	rxp := regexp.MustCompile(filter)
+	for rec := range in {
+		rec.b = rxp.ReplaceAll(rec.b, []byte(""))
+		hashStr := fmt.Sprintf("%x", sha256.Sum256(rec.b))
+		val, loaded := ledger.LoadOrStore(hashStr, rec)
+		rec, ok := val.(*Record)
+		if !ok {
+			errc <- fmt.Errorf("fetch: cast to *Record failed")
 		}
+		if loaded {
+			rec.count++
+		} else {
+			rec.count = 1
+		}
+		out <- rec
 	}
 	close(errc)
 	close(out)
 }
 
 func Fuzz(target string, filter string, wl io.Reader) error {
-	urlchan := make(chan string)
-	recchan := make(chan Record)
+	readout := make(chan *Record)
+	fetchout := make(chan *Record)
+	hashout := make(chan *Record)
 	errchan := make(chan error)
-	go read(strings.Split(target, "{{}}"), bufio.NewScanner(wl), urlchan, errchan)
-	go fetch(filter, urlchan, recchan, errchan)
+	go readproc(strings.Split(target, "{{}}"), bufio.NewScanner(wl), readout, errchan)
+	go fetchproc(readout, fetchout, errchan)
+	go hashproc(filter, fetchout, hashout, errchan)
 
 	for {
 		select {
 		case err := <-errchan:
 			return err
-		case rec := <-recchan:
+		case rec := <-hashout:
 			if rec.count == 1 {
 				fmt.Printf("Url %s generated a unique page hash\n", rec.url)
 			}
--- fuzzup_test.go
+++ fuzzup_test.go
@@ -2,6 +2,7 @@ package fuzzup
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -9,11 +10,16 @@ import (
 	"testing"
 )
 
+func errproc(t *testing.T, c chan error) {
+	for err := range c {
+		t.Fatal(err)
+	}
+}
+
 func TestBuildURL(t *testing.T) {
 	outter := []string{"a", "c", "e", "g"}
 	inner := []string{"b", "d", "f"}
-
-	expected := fmt.Sprintf("%s%s%s%s%s%s%s", outter[0], inner[0], outter[1], inner[1], outter[2], inner[2], outter[3])
+	expected := "abcdefg"
 	result := buildURL(outter, inner)
 	if result != expected {
 		t.Errorf("Expected %s but got %s", expected, result)
@@ -24,34 +30,27 @@ func TestRead(t *testing.T) {
 	const fuzzin = "a\tb\nc\td\ne\tf\n"
 	target := "https://example.com?{{}}=test&test2={{}}"
 	r := bufio.NewScanner(strings.NewReader(fuzzin))
-	out := make(chan string)
+	out := make(chan *Record)
 	errc := make(chan error)
-	go func() {
-		read(strings.Split(target, "{{}}"), r, out, errc)
-		close(errc)
-	}()
-	go func() {
-		for err := range errc {
-			t.Errorf("Got error %s when reading", err.Error())
-		}
-	}()
+	go readproc(strings.Split(target, "{{}}"), r, out, errc)
+	go errproc(t, errc)
 	i := 0
-	for url := range out {
+	for rec := range out {
 		switch i {
 		case 0:
 			const e = "https://example.com?a=test&test2=b"
-			if url != e {
-				t.Errorf("Expected %s got %s", e, url)
+			if rec.url != e {
+				t.Errorf("Expected %s got %s", e, rec.url)
 			}
 		case 1:
 			const e = "https://example.com?c=test&test2=d"
-			if url != e {
-				t.Errorf("Expected %s got %s", e, url)
+			if rec.url != e {
+				t.Errorf("Expected %s got %s", e, rec.url)
 			}
 		case 2:
 			const e = "https://example.com?e=test&test2=f"
-			if url != e {
-				t.Errorf("Expected %s got %s", e, url)
+			if rec.url != e {
+				t.Errorf("Expected %s got %s", e, rec.url)
 			}
 		}
 		i++
@@ -60,49 +59,67 @@ func TestRead(t *testing.T) {
 
 func TestFetch(t *testing.T) {
 	testPages := []string{"Test", "Test2", "uniq", "other"}
+	testPageContent := [][]byte{[]byte("Test"), []byte("Test"), []byte("Test2"), []byte("Test3")}
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		switch incoming := strings.Trim(r.URL.String(), "/"); incoming {
 		case testPages[0]:
-			fmt.Fprintln(w, "Test")
+			fmt.Fprint(w, string(testPageContent[0]))
 		case testPages[1]:
-			fmt.Fprintln(w, "Test")
+			fmt.Fprint(w, string(testPageContent[1]))
 		case testPages[2]:
-			fmt.Fprintln(w, "Test2")
+			fmt.Fprint(w, string(testPageContent[2]))
 		case testPages[3]:
-			fmt.Fprintln(w, "Test3")
+			fmt.Fprint(w, string(testPageContent[3]))
 		default:
 			t.Errorf("Unknown request %s", incoming)
 		}
 	}))
 	defer ts.Close()
-	in := make(chan string)
-	out := make(chan Record)
+	in := make(chan *Record)
+	out := make(chan *Record)
 	errc := make(chan error)
-	go func() {
-		for err := range errc {
-			t.Errorf("%q", err)
-		}
-	}()
-	go fetch("", in, out, errc)
+	go errproc(t, errc)
+	go fetchproc(in, out, errc)
 	go func() {
 		for _, page := range testPages {
-			in <- fmt.Sprintf("%s/%s", ts.URL, page)
+			in <- &Record{url: fmt.Sprintf("%s/%s", ts.URL, page)}
 		}
 		close(in)
 	}()
 	i := 0
 	for r := range out {
-		eurl := fmt.Sprintf("%s/%s", ts.URL, testPages[i])
-		e := Record{url: eurl}
-		switch i {
-		case 0, 2, 3:
-			e.count = 1
-		case 1:
-			e.url = fmt.Sprintf("%s/%s", ts.URL, testPages[0])
-			e.count = 2
+		e := &Record{b: testPageContent[i], url: fmt.Sprintf("%s/%s", ts.URL, testPages[i])}
+		if e.url != r.url || bytes.Compare(e.b, r.b) != 0 {
+			t.Errorf("Expected %q got %q on round %d", e, r, i)
 		}
-		if e != r {
-			t.Errorf("Expected %q got %q", e, r)
+		i++
+	}
+}
+
+func TestHash(t *testing.T) {
+	sample := []string{"Test", "Test", "Test2", "Test3", "Test4"}
+	in := make(chan *Record)
+	out := make(chan *Record)
+	errc := make(chan error)
+	go errproc(t, errc)
+	go hashproc("", in, out, errc)
+	go func() {
+		for _, s := range sample {
+			in <- &Record{b: []byte(s)}
+		}
+		close(in)
+	}()
+	i := 0
+	for r := range out {
+		e := &Record{}
+		switch i {
+		case 1:
+			e.count = 2
+		default:
+			e.count = 1
+		}
+		if r.count != e.count {
+			t.Errorf("Expected %d, got %d", e.count, r.count)
 		}
 		i++
 	}
